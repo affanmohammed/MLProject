2eabd68b2956fed6fa878a3e00bd3ad08a5cc787
Affan <49243518+affanmohammed@users.noreply.github.com>
2024-02-26 07:43:02 +0530

    Update cvstream.py

--- cvstream.py
+++ cvstream.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 run = st.checkbox('Run')
 if run:
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     st.title("test2 ")
     frame_placeholder = st.empty()
     stop_button_pressed = st.button("Stop")
