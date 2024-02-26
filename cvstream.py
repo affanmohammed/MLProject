e9909f3205cf6f8e79167a5b3f95ac5faf25d5ac
Affan <49243518+affanmohammed@users.noreply.github.com>
2024-02-26 07:53:30 +0530

    Update cvstream.py

--- cvstream.py
+++ cvstream.py
@@ -1,20 +1,33 @@
+import matplotlib
+matplotlib.use('Agg')  # Set the backend to Agg before importing cv2
+
 import cv2
 import streamlit as st
 
-run = st.checkbox('Run')
-if run:
-    cap = cv2.VideoCapture(0)
-    st.title("test2 ")
-    frame_placeholder = st.empty()
-    stop_button_pressed = st.button("Stop")
+def main():
+    st.title("OpenCV + Streamlit Live Video Stream Example")
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        cv2.rectangle(frame, (0, 0), (640, 40), (115,115,115), -1)
-        frame_placeholder.image(frame, channels="BGR")
+    run = st.checkbox('Run')
 
-        if stop_button_pressed:
-            break
+    if run:
+        video_capture = cv2.VideoCapture(0)
 
-    cap.release()
-    cv2.destroyAllWindows()
+        while run:
+            ret, frame = video_capture.read()
+
+            if not ret:
+                st.error("Failed to capture frame.")
+                break
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            st.image(frame, channels="RGB", use_column_width=True)
+
+            # Wait for a short time to prevent freezing of the GUI
+            key = cv2.waitKey(30) & 0xff
+            if key == 27:  # Escape key to exit
+                break
+
+        video_capture.release()
+
+if __name__ == "__main__":
+    main()
